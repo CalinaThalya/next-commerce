fc4bb0c9eaaea5a5c4da7bae73afb1d5f454e238
CalinaThalya <calina.thalya@icloud.com>
2024-04-13 13:10:24 -0300

    listagem de prod -part1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 export default function Home() {
   return (
     <div>
-      <div className="max-w-wxl mx-auto pt-8 px-8 xl:px-0">
+      <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
         <div
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1 xl:gab-6
+          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gab-6
         "
         >
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            PROD 1
+          </div>
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            PROD 1
+          </div>
+          <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+            PROD 1
+          </div>
           <div className="bg-white rounded-lg shadow-lg overflow-hidden">
             PROD 1
           </div>
