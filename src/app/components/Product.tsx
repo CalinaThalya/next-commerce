43f984966820a0ade79a0d1e9d6f9f1f3d91b3f9
CalinaThalya <calina.thalya@icloud.com>
2024-04-13 21:11:39 -0300

    layout

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -11,7 +11,12 @@ export default function Products({ product }: ProductProps) {
       <div className="relative max-h-72 flex-1">
         <ProductImage product={product} fill />
       </div>
-      <div className="flex justify-between font-bold my-3">{product.name}</div>
+      <div className="flex justify-between font-bold my-3">
+        <p className="w-40 truncate">{product.name}</p>
+        <p className="text-white bg-gray-600 p-2 rounded-md">{product.price}</p>
+      </div>
+      <p style={{ fontSize: "0.rem" }}>{product.description}</p>
+
       <button className="font-bold rounded-md bg-blue-600 text-white px-3 py-2.5 text-sm text-center flex items-center justify-center space-x-1">
         <img
           src="/8679212_shopping_bag_icon (1).png"
