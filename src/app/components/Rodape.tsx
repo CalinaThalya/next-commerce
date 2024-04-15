6d7f7554bca1fe9528d034e269350dcecaf9d084
CalinaThalya <calina.thalya@icloud.com>
2024-04-15 00:56:15 -0300

    layout

--- src/app/components/Rodape.tsx
+++ src/app/components/Rodape.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-200 text-black py-4 mt-8">
+    <footer className="bg-gray-200 text-black py-2 fixed bottom-0 w-full">
       <div className="container mx-auto">
         <p className="text-sm text-center">
           MKS sistemas © todos direitos reservados
